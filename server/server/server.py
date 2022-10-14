b93080a6904b5a39705bdba0332dd80204136b41
mgabriel6643 <mgabriel6643@gmail.com>
2022-10-14 16:37:29 -0300

    commit test 2

--- server/server/server.py
+++ server/server/server.py
@@ -7,7 +7,7 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 s.bind((HOST,PORT))
 s.listen(1)
-arq = open()
+arq = open('/home/departamentos/financeiro/financeiro.tar.gz', 'r')
 
 for i in arq.readlines():
     s.send(i)
